9a8684c0c1c381c34a6df3dae585e80c545776d0
NguyenTuanEZS <nguyentuan@ezs.vn>
2024-08-13 10:38:20 +0700

    first commit

---
+++ app/src/main/java/vn/cser21/Callback21.java
@@ -0,0 +1,14 @@
+package jonivaspa.ezspa;
+
+public class Callback21 {
+    public Exception lastExp;
+    public Object lastResult;
+
+    public void ok() {
+    }
+
+    public void no() {
+    }
+
+
+}
---
+++ app/src/main/java/vn/cser21/incoming/EndCallEvent.java
@@ -0,0 +1,3 @@
+package jonivaspa.ezspa.incoming;
+
+public class EndCallEvent { }
